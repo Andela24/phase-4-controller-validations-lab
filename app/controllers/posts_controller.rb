2db052c851573d9bd83cb6a0c03a47043c2fc06b
Andela Stevanovic <andjela.s166@gmail.com>
2022-07-28 13:36:14 -0400

    commit tree 14b3f3520ff98de581f7fe36ceca0ca87e0a09c4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,12 +9,15 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
+  rescue ApplicationRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
+
 
   def post_params
     params.permit(:category, :content, :title)
